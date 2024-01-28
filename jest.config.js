a78874c9ec245041482585a74347df81e16b89b3
sudremodel <144292180+sudremodel@users.noreply.github.com>
2024-01-28 13:21:57 -0500

    added unit tests and logger functions

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    testMatch: ["**/__tests__/**/*.js", "**/?(*.)+(spec|test).js"],
+    testPathIgnorePatterns: ["\\\\node_modules\\\\"]
+  };
+  
--- server.js
+++ server.js
@@ -4,13 +4,27 @@ const OpenAI = require("openai");
 const axios = require('axios');
 const mysql = require('mysql2');
 require('dotenv').config();
-const cors = require("cors");
+const winston = require('winston');
 
 const app = express();
-app.use(cors);
 const port = 3020;
 
 app.use(bodyParser.json());
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }), // Log all levels to a combined file
+  ],
+});
+app.use((err, req, res, next) => {
+  logger.error(`Error caught by error handling middleware: ${err.message}`, { error: err });
+  const errorMessage = 'Internal Server Error';
+  res.status(500).json({ error: errorMessage });
+});
 
 const dalleApiKey = process.env.DALLE_API_KEY;
 const shutterstockApiKey = process.env.SHUTTERSTOCK_API_KEY;
@@ -28,8 +42,7 @@ const checkApiKey = (req, res, next) => {
   if (req.path === '/dalle/search' && apiKeyHeader !== dalleApiKey) {
     return res.status(401).json({ error: 'Unauthorized for DALL-E endpoint' });
   }
-
-  if (req.path === '/shutterstock/search' && bearer !== 'Bearer' && apiKeyHeader !== shutterstockApiKey) {
+   if (req.path === '/shutterstock/search' && apiKey !== shutterstockApiKey) {
     return res.status(401).json({ error: 'Unauthorized for Shutterstock endpoint' });
   }
 
@@ -42,7 +55,7 @@ const openai = new OpenAI({
     dangerouslyAllowBrowser: true,
 });
 
-app.post('/dalle/search', checkApiKey, async (req, res) => {
+app.post('/dalle/search', checkApiKey, async (req, res, next) => {
   try {
     const { query } = req.body;
     const image = await openai.images.generate({
@@ -51,12 +64,13 @@ app.post('/dalle/search', checkApiKey, async (req, res) => {
     });
     const imageUrl = image.data[0]?.url;
     res.json({ imageUrl });
+    logger.info('DALL-E image search successful', { query, imageUrl });
   } catch (error) {
-    console.error('Error in DALL-E image search:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    // logger.error('Error in DALL-E image search', { error });
+    next(error);
   }
 });
-app.post('/shutterstock/search',checkApiKey, async (req, res) => {
+app.post('/shutterstock/search',checkApiKey, async (req, res, next) => {
     try {
       const { query } = req.body;
       const response = await axios.get('https://api.shutterstock.com/v2/images/search', {
@@ -72,9 +86,10 @@ app.post('/shutterstock/search',checkApiKey, async (req, res) => {
   
       const imageUrls = response.data.data.map(img => img.assets.preview.url);
       res.json({ imageUrls });
+      logger.info('Shutterstock image search successful', { query, imageUrls });
     } catch (error) {
-      console.error('Error in Shutterstock image search:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
+      // logger.error('Error in Shutterstock image search', { error });
+      next(error);
     }
   });
   const dbConfig = {
@@ -93,19 +108,22 @@ app.post('/shutterstock/search',checkApiKey, async (req, res) => {
   });
   app.post('/storeDownload', async (req, res) => {
     try {
-      const { organization_id, company_id, logoUrl, Headlines, Subheadlines, CallToAction, imageUrl } = req.body;
+      const { organization_id, entity_id, logoUrl, Headlines, Subheadlines, CallToAction, imageUrl } = req.body;
       const createdAtTime = new Date();
+      if (!organization_id || !entity_id || !imageUrl) {
+        return res.status(500).json({ error: 'organization_id, entity_id, and image_url are required fields' });
+      }
       console.log("you are in remodel database");
       const createTableQuery = `
         CREATE TABLE IF NOT EXISTS ads (
           id INT AUTO_INCREMENT PRIMARY KEY,
-          organization_id INT,
-          company_id INT,
+          organization_id INT NOT NULL,
+          entity_id INT NOT NULL,
           logo_url VARCHAR(255),
           Headlines VARCHAR(255),
           Subheadlines VARCHAR(255),
           CallToAction VARCHAR(255),
-          image_url VARCHAR(255),
+          image_url VARCHAR(255) NOT NULL,
           created_at_time TIMESTAMP
         )
       `;
@@ -117,50 +135,8 @@ app.post('/shutterstock/search',checkApiKey, async (req, res) => {
           return;
         }
   
-        const insertQuery = 'INSERT INTO ads (organization_id, company_id, logo_url, Headlines, Subheadlines, CallToAction, image_url, created_at_time) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
-        const values = [organization_id, company_id, logoUrl, Headlines, Subheadlines, CallToAction, imageUrl, createdAtTime];
-  
-        dbConnection.query(insertQuery, values, (error, results) => {
-          if (error) {
-            console.error('Error storing data in the database:', error);
-            res.status(500).json({ error: 'Internal Server Error' });
-          } else {
-            console.log('Data stored successfully');
-            res.json({ success: true });
-          }
-        });
-      });
-    } catch (error) {
-      console.error('Error handling storeData request:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });  
-  app.post('/addToLibrary', async (req, res) => {
-    try {
-      const { organization_id, company_id, logoUrl, imageUrl } = req.body;
-      const createdAtTime = new Date();
-      console.log("you are in remodel database");
-  
-      const createTableQuery = `
-        CREATE TABLE IF NOT EXISTS userLibrary (
-          id INT AUTO_INCREMENT PRIMARY KEY,
-          organization_id INT,
-          company_id INT,
-          logo_url VARCHAR(255),
-          image_url VARCHAR(255),
-          created_at_time TIMESTAMP
-        )
-      `;
-      dbConnection.query(createTableQuery, (error) => {
-        if (error) {
-          console.error('Error creating table:', error);
-          res.status(500).json({ error: 'Internal Server Error' });
-          return;
-        }
-  
-        // Insert data into the 'userLibrary' table
-        const insertQuery = 'INSERT INTO userLibrary (organization_id, company_id, logo_url, image_url, created_at_time) VALUES (?, ?, ?, ?, ?)';
-        const values = [organization_id, company_id, logoUrl, imageUrl, createdAtTime];
+        const insertQuery = 'INSERT INTO ads (organization_id, entity_id, logo_url, Headlines, Subheadlines, CallToAction, image_url, created_at_time) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+        const values = [organization_id, entity_id, logoUrl, Headlines, Subheadlines, CallToAction, imageUrl, createdAtTime];
   
         dbConnection.query(insertQuery, values, (error, results) => {
           if (error) {
@@ -177,7 +153,118 @@ app.post('/shutterstock/search',checkApiKey, async (req, res) => {
       res.status(500).json({ error: 'Internal Server Error' });
     }
   });
+
+  app.get('/getDownloadedImages', async (req, res) => {
+    try {
+      const { entity_id } = req.query;
   
+      if (!entity_id) {
+        return res.status(400).json({ error: 'entity_id parameter is required' });
+      }
+  
+      const query = 'SELECT logo_url, Headlines, Subheadlines, CallToAction, image_url FROM ads WHERE entity_id = ?';
+      const values = [entity_id];
+  
+      dbConnection.query(query, values, (error, results) => {
+        if (error) {
+          console.error('Error retrieving data from userLibrary:', error);
+          res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+          if (results.length === 0) {
+            // No data found for the specified entity_id
+            res.status(404).json({ error: 'Entity not found' });
+          } else {
+            console.log('Data retrieved successfully');
+            res.json({ images: results });
+          }
+        }
+      });
+    } catch (error) {
+      console.error('Error handling getDownloadedImages request:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+   
+  
+
+
+  app.post('/addToLibrary', async (req, res) => {
+    try {
+      const { organization_id, entity_id, logoUrl, imageUrl } = req.body;
+      const createdAtTime = new Date();
+      console.log("you are in remodel database");
+      if (!organization_id || !entity_id || !imageUrl) {
+        return res.status(500).json({ error: 'organization_id, entity_id, and image_url are required fields' });
+      }
+      const createTableQuery = `
+        CREATE TABLE IF NOT EXISTS userLibrary (
+          id INT AUTO_INCREMENT PRIMARY KEY,
+          organization_id INT,
+          entity_id INT,
+          logo_url VARCHAR(255),
+          image_url VARCHAR(255),
+          created_at_time TIMESTAMP
+        )
+      `;
+      dbConnection.query(createTableQuery, (error) => {
+        if (error) {
+          console.error('Error creating table:', error);
+          res.status(500).json({ error: 'Internal Server Error' });
+          return;
+        }
+  
+        // Insert data into the 'userLibrary' table
+        const insertQuery = 'INSERT INTO userLibrary (organization_id, entity_id, logo_url, image_url, created_at_time) VALUES (?, ?, ?, ?, ?)';
+        const values = [organization_id, entity_id, logoUrl, imageUrl, createdAtTime];
+  
+        dbConnection.query(insertQuery, values, (error, results) => {
+          if (error) {
+            console.error('Error storing data in the database:', error);
+            res.status(500).json({ error: 'Internal Server Error' });
+          } else {
+            console.log('Data stored successfully');
+            res.json({ success: true });
+          }
+        });
+      });
+    } catch (error) {
+      console.error('Error handling storeData request:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+  app.get('/getUserLibrary', async (req, res) => {
+    try {
+      const { entity_id } = req.query;
+  
+      if (!entity_id) {
+        return res.status(400).json({ error: 'entity_id parameter is required' });
+      }
+  
+      const query = 'SELECT logo_url, image_url FROM userLibrary WHERE entity_id = ?';
+      const values = [entity_id];
+  
+      dbConnection.query(query, values, (error, results) => {
+        if (error) {
+          console.error('Error retrieving data from userLibrary:', error);
+          res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+          if (results.length === 0) {
+            // No data found for the specified entity_id
+            res.status(404).json({ error: 'Entity not found' });
+          } else {
+            console.log('Data retrieved successfully');
+            res.status(200).json({ images: results });
+          }
+        }
+      });
+    } catch (error) {
+      console.error('Error handling getUserLibrary request:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+  
+  
+module.exports = app;
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
